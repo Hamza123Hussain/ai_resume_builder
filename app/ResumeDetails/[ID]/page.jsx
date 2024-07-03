6e65b69479c47140f5bbec5601177cd4bf67e905
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-07-03 16:54:38 +0500

    Skills And Projects Data Has Been Added

--- app/ResumeDetails/[ID]/page.jsx
+++ app/ResumeDetails/[ID]/page.jsx
@@ -5,6 +5,8 @@ import PersonalDetails from '../../../components/ResumeDetails/PersonalDetails'
 import SummaryDetails from '../../../components/ResumeDetails/SummaryDetails'
 import WorkExperience from '../../../components/ResumeDetails/WorkExperience'
 import EducationDetails from '../../../components/ResumeDetails/EducationDetails'
+import SkillDetails from '../../../components/ResumeDetails/SkillDetails'
+import ProjectDetails from '../../../components/ResumeDetails/ProjectDetails'
 const ResumeDetails = ({ params }) => {
   const [index, setindex] = useState(1)
 
@@ -24,8 +26,9 @@ const ResumeDetails = ({ params }) => {
               Previous
             </button>
             <button
+              disabled={index >= 6}
               onClick={() => setindex((prev) => prev + 1)}
-              className=" py-1 px-4 rounded-lg bg-blue-300 text-white"
+              className=" py-1 px-4 rounded-lg bg-blue-600 text-white disabled:opacity-20"
             >
               Next
             </button>
@@ -35,6 +38,8 @@ const ResumeDetails = ({ params }) => {
         {index == 2 ? <SummaryDetails ID={params.ID} /> : ''}
         {index == 3 ? <WorkExperience ID={params.ID} /> : ''}
         {index == 4 ? <EducationDetails ID={params.ID} /> : ''}
+        {index == 5 ? <SkillDetails ID={params.ID} /> : ''}
+        {index == 6 ? <ProjectDetails ID={params.ID} /> : ''}
       </div>
     </div>
   )
