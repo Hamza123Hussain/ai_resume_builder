882cb02f40ec352e69d431fd548cf823994fecd4
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-07-03 18:00:52 +0500

    Deletion of Data Successful

--- components/InformationLists/WorkExperienceList.jsx
+++ components/InformationLists/WorkExperienceList.jsx
@@ -3,6 +3,7 @@ import React, { useEffect } from 'react'
 import { useState } from 'react'
 import { supabase } from '../../lib/supabaseconfig'
 import { useRouter } from 'next/navigation'
+import { Trash2 } from 'lucide-react'
 const WorkExperienceList = ({ ID }) => {
   const [WorkData, SetData] = useState([])
   const getdata = async () => {
@@ -23,6 +24,24 @@ const WorkExperienceList = ({ ID }) => {
   useEffect(() => {
     getdata()
   }, [])
+  const DeleteData = async (itemid) => {
+    try {
+      const { data, error } = await supabase
+        .from('WorkExperience')
+        .delete()
+        .eq('id', itemid)
+
+      if (error) {
+        console.error('Error inserting data:', error.message)
+        alert('NO DATA SAVED')
+      } else {
+        console.log('Data inserted successfully:', data)
+        getdata()
+      }
+    } catch (err) {
+      console.error('Unexpected error:', err)
+    }
+  }
   const router = useRouter()
 
   return (
@@ -32,22 +51,29 @@ const WorkExperienceList = ({ ID }) => {
           <div
             className="p-3 bg-slate-200 rounded-lg flex flex-col cursor-pointer "
             key={index}
-            onClick={() => router.push(`/WorkExperience/${element.id}`)}
           >
-            <div className=" flex font-bold ">
-              <h1 className=" capitalize">{element.Jobtitle}</h1>,
-              <h1 className=" capitalize">{element.Company}</h1>
+            <div className=" flex justify-between">
+              <div
+                className=" flex font-bold "
+                onClick={() => router.push(`/WorkExperience/${element.id}`)}
+              >
+                <h1 className=" capitalize">{element.Jobtitle}</h1>,
+                <h1 className=" capitalize">{element.Company}</h1>
+              </div>
+              <Trash2 onClick={() => DeleteData(element.id)} />
             </div>
             <div className=" flex justify-between">
-              <div className=" flex gap-2">
-                {' '}
-                <h1 className=" capitalize opacity-75">
-                  {element.StartDate}
-                </h1>-{' '}
+              <div
+                className=" flex gap-2"
+                onClick={() => router.push(`/WorkExperience/${element.id}`)}
+              >
+                <h1 className=" capitalize opacity-75">{element.StartDate}</h1>-
                 <h1 className=" capitalize opacity-75">{element.EndDate}</h1>
               </div>
-              <div className="flex gap-2 opacity-85">
-                {' '}
+              <div
+                className="flex gap-2 opacity-85"
+                onClick={() => router.push(`/WorkExperience/${element.id}`)}
+              >
                 <h1 className=" capitalize">{element.State}</h1>,
                 <h1 className=" capitalize">{element.Country}</h1>
               </div>
